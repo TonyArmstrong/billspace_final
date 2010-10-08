43e4ca19cb23996dd2f397f6093852b5131609f0
Tony Armstrong <anthony.o.armstrong@gmail.com>
2010-10-08 17:01:48 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BillspaceFinal::Application.config.secret_token = '99840669deef346b188a8c633f6524f72fbb0ea06da3fe3ef5dc16155bb43e87da088b1bd8fef76a72726c3f1cdf12a06da7691f9f1ae3e8cbe55d395a8fa194'
